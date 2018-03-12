e4d4fc2fc48adb95268f5fb8ea8b86fe8c83373d
juan <jicibils@hotmail.com>
2018-03-12 10:13:59 -0300

    updated sql

--- gastronomiaWeb.sql
+++ gastronomiaWeb.sql
@@ -2,4 +2,5 @@ drop table if exists publicacion;
 CREATE TABLE publicacion 	(nro_publicacion INTEGER AUTO_INCREMENT NOT NULL,
 						 titulo VARCHAR (100),
 						 descripcion VARCHAR (100),
+						 fecha DATE,
 						 PRIMARY KEY (nro_publicacion));
