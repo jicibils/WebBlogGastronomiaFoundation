0ebca6366a6f22f9a499cf1ada4843c564be6fae
juan <jicibils@hotmail.com>
2018-03-19 18:50:50 -0300

    modularizando api

--- ApiWebGastronomia/recuperarComentarios.php
+++ ApiWebGastronomia/recuperarComentarios.php
@@ -1,23 +1,5 @@
 <?php
-// Parametros a configurar para la conexion de la base de datos
-
-define("DB_HOST", "localhost");
-define("DB_USERNAME", "root");
-define("DB_PASSWORD", "");
-define("DB_DATABASE", "gastronomiaWeb");
-
-// Fin de los parametros a configurar para la conexion de la base de datos
-
-$connect_db = mysqli_connect(DB_HOST , DB_USERNAME, DB_PASSWORD, DB_DATABASE);
-
-if (!$connect_db) {
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    exit;
-}
-
-
+include 'abrirConexion.php';
 
 $sql = "SELECT comentario,tituloPubli FROM comentarios";
 if (!$resultado = $connect_db->query($sql)) {
